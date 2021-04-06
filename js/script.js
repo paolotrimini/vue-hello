5a2cab91c5010cb912f91919a3cbaed5fa90d4e8
paolotrimini <paolo.trimini@gmail.com>
2021-04-06 13:49:29 +0200

    caricamento file

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Stampare a schermo il contenuto di una variabile dentro un h1.
+// BONUS: utilizzare un data come nome del file immagine in un tag img.
+
+
+function initVue() {
+
+}
+
+function init() {
+    initVue();
+
+}
+$(init);
